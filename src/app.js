eca2ba0ca6ba3a5284afe38ff31a5bc47e067264
abombalemba <leaderbomjganga@mail.ru>
2025-12-10 01:06:33 +0300

    fix: upgraded app.js
    - deleted cart object

--- src/app.js
+++ src/app.js
@@ -9,16 +9,14 @@ const urlencodedParser = express.urlencoded({ extended: false });
 const homeRouter = require("./Routes/homeRouter.js");
 const tourRouter = require("./Routes/tourRouter.js");
 const clientRouter = require("./Routes/clientRouter.js");
-const saleRouter = require("./Routes/saleRouter.js");
 
+app.use("/", homeRouter);
 app.use("/tours", urlencodedParser, tourRouter);
 app.use("/clients", urlencodedParser, clientRouter);
-app.use("/sales", urlencodedParser, saleRouter);
-app.use("/", homeRouter);
 
 app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
 
-console.log("Server is ready to Connect...!")
+console.log("app is running on 3000 port..")
 app.listen(3000);
